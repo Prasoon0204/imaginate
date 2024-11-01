ee5c2fbec27bb3296d60e09bcb989c9b44659d77
Prasoon Thakur <prasoon.thakur0204@gmail.com>
2024-11-01 10:22:53 +0530

    initial commit

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <main className='auth'>{children}</main>
+  )
+}
+
+export default layout
---
+++ app/layout.jsx
@@ -0,0 +1,36 @@
+import localFont from "next/font/local";
+import { IBM_Plex_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from '@clerk/nextjs'
+
+import "./globals.css";
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
+});
+
+export const metadata = {
+  title: "Imaginate",
+  description: "AI-Powered image generator",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+    appearance={{
+      variables: { colorPrimary: '#624cf5' },
+      layout: {
+        unsafe_disableDevelopmentModeWarnings: true,
+      },
+    }}
+    >
+    <html lang="en">
+      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+        {children}
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
