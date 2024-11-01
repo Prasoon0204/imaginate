1fb3229697a64f8ede072559dbe2085c823b4219
Prasoon Thakur <prasoon.thakur0204@gmail.com>
2024-11-01 12:15:33 +0530

    change in middleware.js

--- middleware.js
+++ middleware.js
@@ -1,10 +1,14 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware()
+const middleware = clerkMiddleware({
+  publicRoutes: ['/api/webhooks/clerk'],
+});
+
+export default middleware;
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
-}
+};
